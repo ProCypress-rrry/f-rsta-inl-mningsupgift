fde732cb79abb6eda2210a437574431b9433d7a2
vijo07001 <vijo07001@E-PW05CGQX>
2024-09-11 12:20:49 +0200

    Add project files.

---
+++ första inlämningsupgift/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    namespace first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1");
+            Console.WriteLine("du har lärt dig skriva up \" och \\");
+        }
+    }
+}
